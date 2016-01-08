cdc02b13477af97b8abc3d9184369550d6cbf805
Eric Erhardt <eric.erhardt@microsoft.com>
2016-01-08 12:02:56 -0600

    CompareOptions.IgnoreSymbols only ignores punctuation on Unix, but not other symbols
    By default, ICU alternate shifted collation handling only ignores punctuation, not all symbols, so change the "variable top" to include all symbols and currency characters.
    
    Fix #4907

--- src/corefx/System.Globalization.Native/configure.cmake
+++ src/corefx/System.Globalization.Native/configure.cmake
@@ -1,9 +1,16 @@
 include(CheckCXXSourceCompiles)
+include(CheckSymbolExists)
 
 CHECK_CXX_SOURCE_COMPILES("
     #include <unicode/udat.h>
     int main() { UDateFormatSymbolType e = UDAT_STANDALONE_SHORTER_WEEKDAYS; }
 " HAVE_UDAT_STANDALONE_SHORTER_WEEKDAYS)
+
+set(CMAKE_REQUIRED_INCLUDES ${ICU_HOMEBREW_INC_PATH})
+check_symbol_exists(
+    ucol_setMaxVariable
+    "unicode/ucol.h"
+    HAVE_SET_MAX_VARIABLE)
 
 configure_file(
     ${CMAKE_CURRENT_SOURCE_DIR}/config.h.in
