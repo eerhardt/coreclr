8b86eb0118d2a52cbf9093ae3b8f4c335e6d3835
Eric Erhardt <eric.erhardt@microsoft.com>
2016-01-08 12:43:16 -0600

    again

--- src/corefx/System.Globalization.Native/configure.cmake
+++ src/corefx/System.Globalization.Native/configure.cmake
@@ -6,7 +6,7 @@ CHECK_CXX_SOURCE_COMPILES("
     int main() { UDateFormatSymbolType e = UDAT_STANDALONE_SHORTER_WEEKDAYS; }
 " HAVE_UDAT_STANDALONE_SHORTER_WEEKDAYS)
 
-list(APPEND CMAKE_REQUIRED_LIBRARIES icuuc)
+set(CMAKE_REQUIRED_INCLUDES ${ICU_HOMEBREW_INC_PATH})
 check_symbol_exists(
     ucol_setMaxVariable
     "unicode/ucol.h"
